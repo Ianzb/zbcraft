e36faa29f13f248d2434174cb1faa94c7043bec8
Ianzb <93322252@qq.com>
2024-10-15 22:01:59 +0800

    2.0.0-alpha.1

---
+++ src/main/java/org/zb/zbcraft/Zbcraft.java
@@ -0,0 +1,10 @@
+package org.zb.zbcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Zbcraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/zb/zbcraft/client/ZbcraftClient.java
@@ -0,0 +1,10 @@
+package org.zb.zbcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZbcraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
